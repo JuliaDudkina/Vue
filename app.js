45c2947204f92362970b5fbe13f91d56e3926ce7
juliadud <yuliiadudkina@gmail.com>
2021-11-02 12:20:11 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            boxAselected: false,
+            boxBselected: false,
+            boxCselected: false,
+        }
+    },
+    computed:{
+      boxCclasses(){
+          return {active: this.boxCselected}
+      }
+    },
+    methods:{
+        boxSelected(box){
+            if(box === "A"){
+                this.boxAselected = !this.boxAselected;
+            } else if(box === "B"){
+                this.boxBselected = !this.boxBselected;
+            }else if(box === "C"){
+                this.boxCselected = !this.boxCselected;
+            }
+        }
+    }
+});
+app.mount('#styling');
