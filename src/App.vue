7f98c6d4c17a16acdaed45f0f401bff340072310
juliadud <yuliiadudkina@gmail.com>
2021-11-03 16:42:35 +0200

    using provide inject

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <active-element
-      :topic-title="activeTopic && activeTopic.title"
-      :text="activeTopic && activeTopic.fullText"
+        :topic-title="activeTopic && activeTopic.title"
+        :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base></knowledge-base>
   </div>
 </template>
 
@@ -18,18 +18,24 @@ export default {
           title: 'The Basics',
           description: 'Core Vue basics you have to know',
           fullText:
-            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+              'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
         },
         {
           id: 'components',
           title: 'Components',
           description:
-            'Components are a core concept for building Vue UIs and apps',
+              'Components are a core concept for building Vue UIs and apps',
           fullText:
-            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+              'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
         },
       ],
       activeTopic: null,
+    };
+  },
+  provide() {
+    return {
+      topics: this.topics,
+      selectTopic: this.activateTopic
     };
   },
   methods: {
@@ -37,6 +43,16 @@ export default {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
   },
+  mounted() {
+    setTimeout(() => {
+      this.topics.push({
+        id: 'events',
+        title: 'Events',
+        description: 'Events are important in Vue',
+        fullText: 'Events allow you to trigger code on demand!'
+      });
+    }, 3000);
+  }
 };
 </script>
 
