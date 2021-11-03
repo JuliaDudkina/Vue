dac2d8a47d08e4bc2e1ed0b76bff376eb4508fba
juliadud <yuliiadudkina@gmail.com>
2021-11-03 13:05:15 +0200

    props as an obj

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -18,7 +18,33 @@
 
 <script>
 export default {
-  props: [ 'name','phoneNumber','emailAddress', 'isFavorite'],
+  // props: [ 'name','phoneNumber','emailAddress', 'isFavorite'],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+      validator: function (email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      }
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: '0',
+      validator: function (value){
+        return value === '1' || value === '0';
+      }
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
