f0601a76eb471c14b8453b4c9505d16b522bf75e
juliadud <yuliiadudkina@gmail.com>
2021-11-03 18:34:28 +0200

    using component tag instead of v-if

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,32 @@
   <div>
 <!--    <the-header></the-header>-->
     <TheHeader/>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-<!--    <course-goals>-->
-<!--      <template #default="slotProps">-->
-<!--        <h2>{{ slotProps.item }}</h2>-->
-<!--      </template>-->
-<!--    </course-goals>-->
-    <course-goals #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-    </course-goals>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+<!--    <active-goals></active-goals>-->
+<!--    <ManageGoals/>-->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 //Local component
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from "./components/CourseGoals";
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from "./components/CourseGoals";
+import ActiveGoals from "./components/ActiveGoals";
+import ManageGoals from "./components/ManageGoals";
 
 export default {
   components:{
     // 'the-header': TheHeader,
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
@@ -41,8 +36,14 @@ export default {
         description: 'Site owner and admin',
         role: 'admin',
       },
+      selectedComponent: null,
     };
   },
+  methods:{
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
---
+++ src/components/ManageGoals.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>ManageGoals</h2>
+</template>
+
+<script>
+export default {
+  name: "ManageGoals"
+}
+</script>
+
+<style scoped>
+
+</style>
