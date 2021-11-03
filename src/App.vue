86a591e20f8917725b3231d2e08eca9e5c79ae37
juliadud <yuliiadudkina@gmail.com>
2021-11-03 11:34:36 +0200

    adding vue.app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+  <h2>My friends</h2>
+  <ul>
+    <li></li>
+  </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+        friends: [
+          {
+            id: 'john',
+            name: 'John Doe',
+            phone: '53483764093',
+            email: 'ramdom@email.com'
+          },
+          {
+            id: 'kate',
+            name: 'Kate Doe',
+            phone: '543t3764093',
+            email: 'kramdom@email.com'
+          },
+        ]
+    }
+  }
+}
+</script>
